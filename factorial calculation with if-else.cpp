a8ae31ddae47ae42e015d8f2676068d395ef9bda
reyhan8trkkl <94196332+reyhan8trkkl@users.noreply.github.com>
2021-12-10 13:54:44 +0300

    Add files via upload

--- factorial calculation with if-else.cpp
+++ factorial calculation with if-else.cpp
@@ -1,1 +1,21 @@
+#include <stdio.h>
 
+int main(){
+	
+	int sayi,s=0,f=1;
+	
+	printf("Faktoriyelini istediginiz sayiyi girin = ");
+	scanf("%d",&sayi);
+	
+	tkr:
+	s++;
+	
+	if(s<=sayi){
+		f=s*f;
+		goto tkr;
+	}
+	
+	printf("Girdiginiz sayinin faktoriyeli = %d",f);
+	
+	return 0;
+}
