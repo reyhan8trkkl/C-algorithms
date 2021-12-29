c977ecf16c204e8f5edb9f47a4d6bae16ffd3819
reyhan8trkkl <94196332+reyhan8trkkl@users.noreply.github.com>
2021-12-29 22:34:41 +0300

    Add files via upload

---
+++ ucgenin alani.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+	
+	int kenar1,kenar2,alan;
+	
+	printf("1.kenari girin:");
+	scanf("%d",&kenar1);
+	printf("2.kenari girin:");
+	scanf("%d",&kenar2);
+	
+	alan = (kenar1*kenar2)/2 ;
+	
+	printf("ucgenin alani=%d",alan);
+	
+	
+	
+	
+	return 0;
+}
