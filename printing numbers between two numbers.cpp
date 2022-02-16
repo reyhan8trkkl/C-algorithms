56c504efd6aa1f0e9327a8ce428cce192e320564
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-02-16 19:24:20 +0300

    Add files via upload

---
+++ printing numbers between two numbers.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+int main(){
+	
+	int first,last,sum;
+	
+	printf("Enter beginning number: ");
+	scanf("%d",&first);
+	
+	printf("Enter ending number: ");
+	scanf("%d",&last);
+	
+	 for(;first<=last;first++){
+	 	
+	 	printf("%d ",first);
+	 	
+	 }
+	
+	
+	return 0;
+}
