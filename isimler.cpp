bb3e147e384327014b97fb632a30544ea2d08520
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-03-04 18:02:29 +0300

    Add files via upload

---
+++ isimler.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+   string isimler[5] = {"Reyhan","Gamze","Seher","Esra","Irem"};
+
+	cout<<isimler[3]<<endl;
+	system("pause");
+	
+	
+	return 0;
+}
