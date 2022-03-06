f6bb2ef73f239f18dfa01124cb5f07e56b825c99
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-03-06 19:26:52 +0300

    Add files via upload

---
+++ math-random.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cmath>
+#include<cstdlib>
+
+using namespace std;
+
+int main(){
+	
+	cout << sqrt(4) <<endl; //karekok
+	cout <<pow(2,4) <<endl; //us alma 2 uzeri 4
+	cout << abs(-10) <<endl; //mutlak deger
+	cout << sqrt(pow(4,2)) << endl;
+    cout << rand()%10;
+	
+	return 0;
+}
+
