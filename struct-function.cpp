57c7c74e1b1e01b0cef312dd149848e44ada6784
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-03-08 19:24:45 +0300

    Add files via upload

---
+++ struct-function.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+struct Person {
+    char name[50];
+    int age;
+    float salary;
+};
+
+void displayData(Person);
+
+int main() {
+    Person p;
+
+    cout << "Enter Full name: ";
+    cin.get(p.name, 50);
+    cout << "Enter age: ";
+    cin >> p.age;
+    cout << "Enter salary: ";
+    cin >> p.salary;
+
+    displayData(p);
+
+    return 0;
+}
+void displayData(Person p) {
+    cout << "\nDisplaying Information.\n";
+    cout << "Name: " << p.name << endl;
+    cout <<"Age: " << p.age << endl;
+    cout << "Salary: " << p.salary;
+}
