58a9c4c045dca0476896de7a9ce943af1e4fdcdd
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-03-17 21:35:28 +0300

    Add files via upload

---
+++ strtol.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+    int base = 10;
+    char str[] = "27ab_1x";
+    char *end; 
+    long int num;
+    
+    num = strtol(str, &end, base);
+    cout << "Number in  String = " << str << endl;
+    cout << "Number in Long Int = " << num << endl;
+    cout << "End String = " << end << endl << endl;
+    
+    strcpy(str, "27");
+    cout << "Number in  String = " << str << endl;
+    num = strtol(str, &end, base);
+    cout << "Number in Long Int = " << num << endl;
+    if (*end) {
+        cout << end;
+    } else {
+        cout << "Null pointer";
+    }
+    return 0;
+}
