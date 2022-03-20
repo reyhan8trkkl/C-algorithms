95f38f39a12712da57aa014c3a710ace7dfdf1d9
Reyhan Türkkal <94196332+reyhan8trkkl@users.noreply.github.com>
2022-03-20 23:47:02 +0300

    Add files via upload

---
+++ abstraction-2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <math.h>
+using namespace std ;
+int main ()
+{
+int x = 8 ;
+int power = 4 ;
+int result = pow ( x , power ) ;        
+std :: cout << " The square of x is : " << result <<  endl ;
+return 0 ;
+}
